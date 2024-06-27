9b02ab073e6b9ab47c23f9946c6ba391b5c7ac24
bambloo <liugchome2004@163.com>
2024-06-27 11:29:59 +0800

    change default config

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const MINER_CONFIG = {
     APP_BASE : __dirname,
     BLOOM_LOCATION: "M:/website.bloom",
     WEBSITE_CACHE_PATH: "M:/miner-website-cache/",
-    MINER_COUNT: 16,
-    WORK_PER_MINER: 64,
+    MINER_COUNT: 32,
+    WORK_PER_MINER: 32,
     MAX_PER_HOST: 128,
     ENTRY_DOMAIN: "http://www.baidu.com"
 }
